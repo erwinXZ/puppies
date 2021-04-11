5b5cd0f5bd751e282bb8f44add17faea28013a21
Erwin Mendez <erwinxyz1@gmail.com>
2021-04-11 13:07:37 -0400

    Added tile styles

--- src/app/public/adoption/adoption.module.ts
+++ src/app/public/adoption/adoption.module.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AdoptionComponent } from './adoption.component';
 import { ADOPTION_ROUTE } from './adotion.route';
+import { PetTileComponent } from './pet-tile/pet-tile.component';
 
 
 
 @NgModule({
-  declarations: [AdoptionComponent],
+  declarations: [AdoptionComponent, PetTileComponent],
   imports: [
     SharedModule,
     RouterModule.forChild([ADOPTION_ROUTE])
---
+++ src/app/public/adoption/pet-tile/pet-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-tile',
+  templateUrl: './pet-tile.component.html',
+  styleUrls: ['./pet-tile.component.css']
+})
+export class PetTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
