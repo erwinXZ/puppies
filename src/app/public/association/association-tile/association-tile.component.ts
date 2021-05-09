de24cc1acc2b2fb05b3864b6d42823469ef312d6
Amancaya <koda01996@gmail.com>
2021-05-08 23:21:18 -0400

    association tile created

---
+++ src/app/public/association/association-tile/association-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-association-tile',
+  templateUrl: './association-tile.component.html',
+  styleUrls: ['./association-tile.component.css']
+})
+export class AssociationTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
