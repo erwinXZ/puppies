a6006e852c1cd50dba22c1044f2926981106dc1a
Erwin Mendez <erwinxyz1@gmail.com>
2021-05-10 00:11:35 -0400

    Changed constant names

--- src/app/shared/constants/date-format.constants.ts
+++ src/app/shared/constants/date-format.constants.ts
@@ -3,4 +3,4 @@ export const DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm';
 export const DATE_TIME3_FORMAT = 'YYYY-MM-DDTHH:mm:ssZ';
 
 export const YEAR_DEFAULT_VALUE = 1;
-export const MONTH_DEFUALT_VALUE = 1;
+export const MONTH_DEFAULT_VALUE = 1;
