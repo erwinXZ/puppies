a10750c1fcf96dc9ad552f7f051722718a6cb80e
Erwin Mendez <erwinxyz1@gmail.com>
2021-05-16 11:16:07 -0400

    Deleted unused component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BooleanTranslatePipe } from './util/boolean-translate';
 import { SharedLibsModule } from './shared-libs.module';
-import { CustomSvgComponent } from './components/custom-svg/custom-svg.component';
 
 @NgModule({
   imports: [
     SharedLibsModule
   ],
   declarations: [
-    BooleanTranslatePipe,
+    BooleanTranslatePipe
   ],
   exports: [
     SharedLibsModule,
