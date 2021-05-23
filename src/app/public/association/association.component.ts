e9a8570dea5689e362c470b21b363678969ac55b
Kaya <koda01996@gmail.com>
2021-05-23 09:21:40 -0400

    PAW-11 Create association component (#24)
    Consuming data from firebase

--- src/app/public/association/association.component.ts
+++ src/app/public/association/association.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IRefuge } from 'src/app/shared/model/refuge.model';
+import { AssociationService } from './association.service';
 
 @Component({
   selector: 'app-association',
   templateUrl: './association.component.html',
   styleUrls: ['./association.component.css']
 })
-export class AssociationComponent implements OnInit {
+export class AssociationComponent implements OnInit, OnDestroy {
+  eventSubscriber: Subscription = new Subscription();
+  refuges: IRefuge[];
 
-  constructor() { }
+  constructor(protected associationService: AssociationService) {}
 
   ngOnInit(): void {
+    this.loadRefuges();
   }
 
+  ngOnDestroy() {
+    this.eventSubscriber.unsubscribe();
+  }
+
+  loadRefuges() {
+    this.eventSubscriber.add(
+      this.associationService.findAll().subscribe(res => {
+        this.refuges = res;
+      })
+    );
+  }
 }
