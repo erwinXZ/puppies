30ea824fb8d90f9facc6d766dc16e711a3f544e5
Amancaya <koda01996@gmail.com>
2021-06-05 22:22:30 -0400

    background color style added

--- src/app/public/association/association-detail/counter/counter.component.ts
+++ src/app/public/association/association-detail/counter/counter.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { IAssociationCounter } from 'src/app/shared/model/association-counter.model';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.scss']
 })
 export class CounterComponent implements OnInit {
   @Input() counter: IAssociationCounter;
+  @Input() backgroundColor: string;
 
   constructor() {}
 
